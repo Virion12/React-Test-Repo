ae762a09ce86f762ae32ce55cfd072440b7564cd
Virion12 <dgiwebapps@gmail.com>
2025-11-24 20:32:28 +0100

    Api : Register endpoint

--- List_manager_api/Controllers/UserController.cs
+++ List_manager_api/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using List_manager_api.Domain.Dto.Auth;
 using List_manager_api.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace List_manager_api.Controllers
 {
@@ -35,10 +36,33 @@ namespace List_manager_api.Controllers
             var newToken = _tokenService.GenerateToken(user);
             var refreshTokenRaw = await _tokenService.GenerateRefreshToken(user);
 
-
-
-
             return Ok(new { userId = user.Id, refreshToken = refreshTokenRaw, accessToken = newToken });
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest registerDetails)
+        {
+
+            User? user = await _db.Users.FirstOrDefaultAsync(u => u.Email == registerDetails.Email);
+            if (user != null)
+            {
+                return BadRequest("User with this email is already registered");
+            }
+
+            if (registerDetails.Password == null || registerDetails.Password == string.Empty) {
+                return BadRequest("Invalid password format");
+            }
+            var passwordHashed = BCrypt.Net.BCrypt.HashPassword(registerDetails.Password);
+
+            user = new User { Email = registerDetails.Email, Name = registerDetails.Name, PasswordHashed = passwordHashed};
+            
+            _db.Users.Add(user);
+            await _db.SaveChangesAsync();
+
+            var accessToken = _tokenService.GenerateToken(user);
+            var refreshToken = await _tokenService.GenerateRefreshToken(user);
+
+            return Ok(new { userId = user.Id, refreshToken = refreshToken, accessToken = accessToken});
+
         }
     }
 }
